6a8559809c44c23dd4daae1507f01e9645bcc7e7
olearczuk <olearczuk.szymon@gmail.com>
2024-05-15 17:06:08 +0200

    install_external_dependenceis and setup_symlinks scripts

--- install_external_dependencies.sh
+++ install_external_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ln -s $PWD/nvim $HOME/.config/nvim
-ln -s $PWD/home/gitconfig $HOME/.gitconfig
-
 sh -c "$(curl -fsSL https://install.ohmyz.sh)"
 mv $HOME/.zshrc $HOME/.oh-my-zshrc
 ln -s $PWD/home/zshrc $HOME/.zshrc
+
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
