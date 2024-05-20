3102776ddbcedda57d8b89d9bed2ec9dae214d26
olearczuk <olearczuk.szymon@gmail.com>
2024-05-21 00:55:27 +0200

    vi-mode plugin in .zshrc, install_external_dependencies.sh fix

--- install_external_dependencies.sh
+++ install_external_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://install.ohmyz.sh)"
-mv $HOME/.zshrc $HOME/.oh-my-zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+rm $HOME/.zshrc
 ln -s $PWD/home/zshrc $HOME/.zshrc
 
 brew tap homebrew/cask-fonts
