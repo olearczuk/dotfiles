9699989433bde558f5725917907c4e8fe831b670
olearczuk <olearczuk.szymon@gmail.com>
2026-09-19 14:23:13 +0400

    MeanderingProgrammer/render-markdown.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -914,6 +914,11 @@ require('lazy').setup({
     event = 'InsertEnter',
     config = true,
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {},
+  },
   -- Te following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
